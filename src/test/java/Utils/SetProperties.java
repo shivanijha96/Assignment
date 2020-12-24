52b07b24808678b8916f7ec814d573c4e05ce3f7
shivanijha96 <75379321+shivanijha96@users.noreply.github.com>
2020-12-24 14:16:17 +0530

    Add files via upload

---
+++ src/test/java/Utils/SetProperties.java
@@ -0,0 +1,29 @@
+package Utils;
+
+import java.io.File;
+
+public class SetProperties {
+
+	// create reference of ReadProperties class.
+	public static ReadProperties appConfig;
+
+
+	public SetProperties() {
+		try {
+			appConfig = new ReadProperties();
+	
+
+			// Read appConfig properties file
+			appConfig.readFile(new File(System.getProperty("user.dir") + "/AmazonApp-Appium/src/test/java/PropertyFiles/appConfig.properties"));
+
+			
+			} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public static ReadProperties getObjectReference(String objReference) {
+
+		throw new IllegalArgumentException("Selected object doesn't exist");
+	}
+}
